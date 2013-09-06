05a5abc016f4a1926ebb0ece96607d025a3984ab
C.Li <chrislitex@gmail.com>
2013-09-06 13:38:06 +0800

    frontend 2

--- app/controllers/recipes.rb
+++ app/controllers/recipes.rb
@@ -1,27 +1,10 @@
 Eater::App.controllers :recipes do
+  layout :recipes
 
   get :index do
     @recipes = Recipe.last(15)
 
     render 'recipes/index'
-  end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-
-  get :index do
-
   end
 
 end
